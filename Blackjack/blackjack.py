b445419c634d8aea0390a0c893661c5e61ed56d3
SavvyHex <paisaketh@gmail.com>
2022-11-25 19:34:48 +0530

    modified:   Blackjack/blackjack.py

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -9,14 +9,25 @@ from hand import Hand
 class Blackjack:
     def __init__(self) -> None:
         self.deck = Deck()
-        self.player, self.comp = Hand(), Hand()
+        self.player, self.dealer = Hand(), Hand()
 
     def drawCard(self, forPlayer:bool=True) -> None:
         if self.deck.is_empty():
             self.deck = Deck()
+            print("New deck!")
         card = choice(self.deck.deck)
         if forPlayer:
             self.player.draw(card)
         else:
-            self.comp.draw(card)
+            self.dealer.draw(card)
         self.deck.remove(card)
+
+    def new_round(self) -> None:
+        for i in range(2):
+            self.drawCard()
+            self.drawCard(False)
+
+        print("Dealer : ")
+        print(self.dealer.hand[0])
+        print("You : ")
+        print(self.player.hand[0], self.player.hand[1])
